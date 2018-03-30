bc453c7da9675a0ff76e51582f17376083f03809
dtcrowley <crowleyd1988@gmail.com>
2018-03-30 10:05:12 -0400

    Removed connection end to allow continued purchasing without restarting entire process

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "@g5Ms2488",
     database: "bamazonDB"
 });
 
@@ -56,14 +56,16 @@ function purchaseItem(){
         } 
     ]).then(function(answer,results){
         var chosenItem = answer.itemID;
-        // console.log(chosenItem);
+        console.log(chosenItem);
         var chosenQuantity = answer.quantity;
-        // console.log(chosenQuantity);
+        console.log(chosenQuantity);
         connection.query("SELECT * FROM products WHERE?", [{item_id: answer.itemID}], function(err, results){
-            if (err) throw err;
+            if (err) {
+                console.log(answer.itemID);
+            }
             var itemPrice = results[0].price;
             var originalInventory = results[0].stock_quantity;
-            var newInventory = originalInventory - answer.quantity;
+            var newInventory = originalInventory - chosenQuantity;
                 console.log("\nInventory level prior to your purchase:\n" + originalInventory + "\n");
             if (chosenQuantity < originalInventory) {
                 console.log("New inventory levels after your purchase:\n" + newInventory + "\n");
@@ -72,9 +74,9 @@ function purchaseItem(){
             }
             else {
                 console.log("Sorry! We don't have enough of that item left to fill your order. Please restart your order and choose a lower quantity to complete purchase.\n")
-                purchaseItem();
             }
-            connection.end();
+
+            purchaseItem();
         })
     })
 }
